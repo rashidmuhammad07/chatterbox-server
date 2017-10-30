841d933fafd0989ae03e96ecbfdd1c3552444d4d
rashidmuhammad07 <mrashid@ucdavis.edu>
2017-10-30 15:30:48 -0700

    Get server running and add client files.

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,4 +1,6 @@
 /* Import node's http module: */
+var handleRequest = require('./request-handler.js');
+console.log(handleRequest);
 var http = require('http');
 
 
@@ -22,7 +24,7 @@ var ip = '127.0.0.1';
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest.requestHandler);
 console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
 
